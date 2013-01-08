3fb06d1d331e648067a0f9e99b8aee83c7e66cb0
Sanja S <sanjascepanovic@gmail.com>
2013-01-08 12:21:13 +0200

    ok version to find avg calling fq per subpref and plot them and some high/medium/low frequency users, however, important here that we obtain quite high overall fq, lets see how does it reflect in the fq pdf plot

--- SET3/visualize/by_calling_fq/map_users.py
+++ SET3/visualize/by_calling_fq/map_users.py
@@ -7,7 +7,6 @@ Created on Jan 2, 2013
 # low: yellow, medium: green and high: red
 # we want to see if there is any spatial distribution, or perhaps some other related to the calling fq
 
-import shapefile
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -23,7 +22,7 @@ f_fq_orig = open(file_name_fq_orig, 'r')
 file_name_fq = "/home/sscepano/D4D res/ORGANIZED/SET3/Distr of Num and Fq of Calls/Usr_div_by_call_fq python.tsv"
 f_fq = open(file_name_fq, 'r')
 
-file_name_subpref = '/home/sscepano/D4D res/ORGANIZED/SET3/Users_divided_by_home_subprefALL.tsv'
+file_name_subpref = '/home/sscepano/D4D res/ORGANIZED/SET3/Night Homes/Subprefs_and_their_users.tsv'
 f_subpref = open(file_name_subpref, 'r')
 
 # read users and their exact fq
@@ -42,6 +41,15 @@ for line in f_fq:
     fq = fq[:-1]
     users_by_calling_fq[usr] = fq
 
+# we will save in a file as well on maps the avg calling fq for that subpref
+file_name_subpref_fq = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/1/Subpref_avg_calling_fq.tsv"
+f_subpref_fq = open(file_name_subpref_fq,'w')
+total_usrs = 0
+total_avg_fq = 0
+total_avg_fq_1 = 0
+# here we do the calculation of avg fq for subprefs
+# we read line from file with subpref and its users
+# and then sum the fq and divide by the number of users in the subpref
 users_by_calling_fq_per_subpref = {}
 subpref_avg_fq = n.zeros(256)
 for subpref in range(1, 256):
@@ -50,21 +58,35 @@ for line in f_subpref:
     avg_fq = 0
     line_elems = line.split('\t')
     subpref = int(line_elems[0][:-1])
+    # we skip -1 as we don't know 'where' is this subpref
     if subpref <> -1:
         users_by_calling_fq_per_subpref.keys().append(subpref)
-        for i in range (1, len(line_elems)-2):
+        for i in range (1, len(line_elems)):
             usr = int(line_elems[i])
             fq = users_by_calling_fq[usr]
             fq_orig = users_calling_fq[usr]
             avg_fq += fq_orig
             users_by_calling_fq_per_subpref[subpref][fq] += 1
-        print line_elems[len(line_elems)-2]
-        usr = int(line_elems[len(line_elems)-2])
-        fq = users_by_calling_fq[usr]
-        fq_orig = users_calling_fq[usr]
-        avg_fq += fq_orig
+            total_avg_fq += fq_orig
         users_by_calling_fq_per_subpref[subpref][fq] += 1
         subpref_avg_fq[subpref] = avg_fq / float((len(line_elems)-1))
+        f_subpref_fq.write(str(subpref) + '\t' + str(avg_fq / float((len(line_elems)-1))) + '\n')
+        total_usrs += (len(line_elems)-1)
+    else:
+        # this is just for check that we have all users
+        minus_one_users = len(line_elems)-1
+        fq_orig = users_calling_fq[usr]
+        total_avg_fq_1 += fq_orig
+
+total_avg_fq_1 += total_avg_fq
+# print total_avg_fq   
+total_avg_fq = total_avg_fq / total_usrs
+print ("Total weighted fq ", total_avg_fq)
+print ("Total users we counted (except subpref -1) ", total_usrs)
+print ("Users in subpref -1 just for check: ", minus_one_users)
+print total_usrs + minus_one_users
+total_avg_fq_1 = total_avg_fq_1 / (total_usrs + minus_one_users)
+print ("Just for check total avg fq adding -1 subpref users ", total_avg_fq_1)
     
 file_name2 = "/home/sscepano/DATA SET7S/D4D/SUBPREF_POS_LONLAT.TSV"
 f2 = open(file_name2, 'r')
@@ -95,10 +117,10 @@ m = Basemap(llcrnrlon=-9, \
                 lat_0 = 7.38, \
                 lon_0 = -5.30)
 
-figure_name_h = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/High_fq_callers_percent.png"
-figure_name_m = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/Medium_fq_callers_percent.png"
-figure_name_l = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/Low_fq_callers_percent.png"
-figure_name_avg = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/Avg_calling_fq.png"
+figure_name_h = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/1/High_fq_callers_percent.png"
+figure_name_m = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/1/Medium_fq_callers_percent.png"
+figure_name_l = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/1/Low_fq_callers_percent.png"
+figure_name_avg = "/home/sscepano/D4D res/allstuff/SET3 frequent callers from python/map by fq calling/1/Avg_calling_fq.png"
 print(figure_name_h,figure_name_m,figure_name_l,figure_name_avg)
     
 
@@ -204,7 +226,7 @@ s = m.readshapefile('/home/sscepano/DATA SET7S/D4D/SubPrefecture/GEOM_SOUS_PREFE
 
 
 
-####################TEST#############################################################################
+####################TEST for shapefile manipulation#############################################################################
 #from matplotlib.collections import LineCollection
 #from matplotlib import cm
 #from shapelib import ShapeFile
