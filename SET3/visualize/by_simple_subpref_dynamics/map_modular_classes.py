e83598c7111c6fec80b3db538539e35ef93c184e
Sanja S <sanjascepanovic@gmail.com>
2013-01-23 15:46:53 +0200

    we have now modular gephi classes potted on a map and we finaly see some nice res

---
+++ SET3/visualize/by_simple_subpref_dynamics/map_modular_classes.py
@@ -0,0 +1,97 @@
+'''
+Created on Jan 23, 2013
+
+@author: sscepano
+'''
+import networkx as nx
+
+G = nx.read_gml("/home/sscepano/D4D res/allstuff/simple dynamics/gml gephi/mod classes2.gml")
+col = [str]*256
+
+for i in range(256):
+    col[i] = 'w'
+
+for node in G.nodes_iter(data=True):
+    #print node[1]['label']
+    col[int(float(node[1]['label']))] = node[1]['graphics']['fill']
+    
+    
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+import matplotlib as mpl
+mpl.rcParams['font.size'] = 10.
+mpl.rcParams['axes.labelsize'] = 8.
+mpl.rcParams['xtick.labelsize'] = 6.
+mpl.rcParams['ytick.labelsize'] = 6.
+
+from shapelib import ShapeFile
+import dbflib
+from matplotlib.collections import LineCollection
+from matplotlib import cm
+
+
+###########################################################################################
+
+fig = plt.figure(3)
+#Custom adjust of the subplots
+plt.subplots_adjust(left=0.05,right=0.95,top=0.90,bottom=0.05,wspace=0.15,hspace=0.05)
+ax = plt.subplot(111)
+
+m = Basemap(llcrnrlon=-9, \
+                llcrnrlat=3.8, \
+                urcrnrlon=-1.5, \
+                urcrnrlat = 11, \
+                resolution = 'h', \
+                projection = 'tmerc', \
+                lat_0 = 7.38, \
+                lon_0 = -5.30)
+
+m.drawcoastlines()
+
+
+shp = ShapeFile(r'/home/sscepano/DATA SET7S/D4D/SubPrefecture/GEOM_SOUS_PREFECTURE')
+dbf = dbflib.open(r'/home/sscepano/DATA SET7S/D4D/SubPrefecture/GEOM_SOUS_PREFECTURE')
+
+msg = "Out of bounds"
+color_col = []
+
+for npoly in range(shp.info()[0]):
+    shpsegs = []
+    shpinfo = []
+    
+    
+    shp_object = shp.read_object(npoly)
+    verts = shp_object.vertices()
+    rings = len(verts)
+    for ring in range(rings):
+        lons, lats = zip(*verts[ring])
+#        if max(lons) > 721. or min(lons) < -721. or max(lats) > 91. or min(lats) < -91:
+#            raise ValueError,msg
+        x, y = m(lons, lats)
+        shpsegs.append(zip(x,y))
+        if ring == 0:
+            shapedict = dbf.read_record(npoly)
+        #print shapedict
+        name = shapedict["ID_DEPART"]
+        subpref_id = shapedict["ID_SP"]
+        #color_col
+        
+        # add information about ring number to dictionary.
+        shapedict['RINGNUM'] = ring+1
+        shapedict['SHAPENUM'] = npoly+1
+        shpinfo.append(shapedict)
+    #print subpref_id
+    #print name
+    lines = LineCollection(shpsegs,antialiaseds=(1,))
+    lines.set_facecolors(col[subpref_id])
+    lines.set_edgecolors('k')
+    lines.set_linewidth(0.3)
+    ax.add_collection(lines)
+    
+plt.savefig('/home/sscepano/D4D res/allstuff/simple dynamics/groups_of_subprefs_by_overall_dynamics.png',dpi=1000)
+#plt.show()
+
+###################################################################################################3
+            
